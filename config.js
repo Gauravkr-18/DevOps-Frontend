faea86595442257aadec354ea4d16ea49a04a909
Gaurav kumar <kumargaurav933466@gmail.com>
2025-10-16 00:15:05 +0530

    updated

--- config.js
+++ config.js
@@ -2,14 +2,14 @@
 // This file is automatically updated during deployment
 
 // Production backend URL (will be replaced during deployment)
-window.BACKEND_URL = 'https://your-backend-domain.com/api';
+window.BACKEND_URL = 'http://65.0.5.49/api';
 
 // Environment-specific configurations
 if (window.location.hostname.includes('staging')) {
-    window.BACKEND_URL = 'https://staging-backend-domain.com/api';
+    window.BACKEND_URL = 'http://65.0.5.49/api';
     console.log('Staging environment detected');
 } else if (window.location.hostname.includes('dev')) {
-    window.BACKEND_URL = 'https://dev-backend-domain.com/api';
+    window.BACKEND_URL = 'http://65.0.5.49/api';
     console.log('Development environment detected');
 } else {
     console.log('Production environment detected');
